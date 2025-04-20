1249a70bbd7a5b5156c65bc207569677edb3ba50
Vaibhav Inzil <vaibhavinzil@gmail.com>
2025-04-20 17:01:27 +0530

    Initial commit

---
+++ StyleHaven v2/stylehaven-backend/controllers/authController.js
@@ -0,0 +1,38 @@
+const User = require('../models/User');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+
+// Login controller
+exports.login = async (req, res) => {
+  const { username, password } = req.body;
+
+  try {
+    const user = await User.findOne({ username });
+    if (!user) {
+      return res.status(400).json({ message: 'Invalid credentials' });
+    }
+
+    // Compare password
+    // Instead of this:
+// const isMatch = await bcrypt.compare(password, user.password);
+
+// Do this:
+const isMatch = password === user.password;
+
+    if (!isMatch) {
+      return res.status(400).json({ message: 'Invalid credentials' });
+    }
+
+    // Generate JWT token
+    const token = jwt.sign(
+      { userId: user._id },
+      process.env.JWT_SECRET,
+      { expiresIn: '1h' }
+    );
+
+    res.json({ token });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ message: 'Server error' });
+  }
+};
